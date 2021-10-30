c9500181477845cbec3cf41a0fe477736c8a0c9f
Defmc <defmc0101@gmail.com>
2021-10-29 21:37:33 -0400

    perf(cat): use memory mapped files

--- src/cmd/cat.rs
+++ src/cmd/cat.rs
@@ -1,21 +1,22 @@
 use crate::cmd::ProgResult;
-use std::fs;
+use std::fs::File;
 use std::io::{stdout, Write};
+
+use memmap2::MmapOptions;
 
 pub fn cat(paths: &[String]) -> ProgResult {
     let out = stdout();
     let mut out = out.lock();
 
-    write!(
-        out,
-        "{}",
-        paths
-            .iter()
-            .skip(1)
-            .map(|path| fs::read_to_string(path).expect("Cannot extract file content"))
-            .collect::<String>()
-    )?;
-
+    paths.iter().skip(1).for_each(|path| {
+        let buf_reader = unsafe {
+            MmapOptions::new()
+                .map(&File::open(path).expect("File not found"))
+                .unwrap()
+        };
+        out.write_all(&buf_reader[..])
+            .expect("Cannot show file content");
+    });
     out.flush()?;
     Ok(())
 }
