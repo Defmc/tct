81e9f86951b3fae61ceee65613d26325d30abb04
Defmc <defmc0101@gmail.com>
2021-11-02 00:47:57 -0400

    perf(grep): use linear index search (without inline chunks system)

--- src/cmd/grep.rs
+++ src/cmd/grep.rs
@@ -1,7 +1,6 @@
 use crate::cmd::ProgResult;
 use std::fs::File;
 use std::io::{stdout, StdoutLock, Write};
-use std::ops::ControlFlow;
 
 use memmap2::MmapOptions;
 
@@ -11,9 +10,10 @@ pub fn grep(items: &[String]) -> ProgResult {
 
     let pattern = items[1].as_bytes();
 
-    items.iter().skip(2).for_each(|filepath| {
-        file_grep(&mut out, pattern, filepath);
-    });
+    items
+        .iter()
+        .skip(2)
+        .for_each(|filepath| file_grep(&mut out, pattern, filepath));
     Ok(())
 }
 
@@ -21,30 +21,28 @@ pub fn grep(items: &[String]) -> ProgResult {
 pub fn file_grep(out: &mut StdoutLock, pattern: &[u8], filepath: &str) {
     let buf_reader = unsafe {
         MmapOptions::new()
-            .map(&File::open(filepath).expect("File not fond"))
+            .map(&File::open(filepath).expect("File not found"))
             .expect("Cant map file")
     };
-    buf_reader.split(|&x| x == b'\n').for_each(|line| {
-        if contains_slice(line, pattern) {
-            out.write_all(line).expect("Cannot print");
-            out.write_all(b"\n").unwrap();
-        }
-    })
-}
 
-pub fn contains_slice(lhs: &[u8], rhs: &[u8]) -> bool {
-    match lhs.iter().try_fold(0usize, |indx_seq, &c| {
-        if c == rhs[indx_seq] {
-            if indx_seq == rhs.len() - 1 {
-                ControlFlow::Break(())
+    let (mut line_start, mut indx_seq) = (0, 0);
+    buf_reader.iter().enumerate().for_each(|(i, &b)| {
+        if indx_seq != pattern.len() {
+            if b == pattern[indx_seq] {
+                indx_seq += 1;
             } else {
-                ControlFlow::Continue(indx_seq + 1)
+                indx_seq = 0;
             }
-        } else {
-            ControlFlow::Continue(0)
         }
-    }) {
-        ControlFlow::Break(_) => true,
-        ControlFlow::Continue(_) => false,
-    }
+
+        if b == b'\n' {
+            if indx_seq == pattern.len() {
+                out.write_all(&buf_reader[line_start..i])
+                    .expect("Cannot print");
+                out.write_all(b"\n").unwrap();
+                indx_seq = 0;
+            }
+            line_start = i + 1;
+        }
+    });
 }
